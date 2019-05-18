d2138c370a2d7a7484da3151af9ada08f65cf4dc
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-05-18 14:17:39 +0400

    Map and Reduce

---
+++ Map and Reduce/MapReduce.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_And_Reduce
+{
+   static class MapReduce
+    {
+        public static List<T> Map<T,Q>(this List<Q> list,Func<Q,T> func)
+        {
+            List<T> result = new List<T>();
+            foreach( Q item in list)
+            {
+                result.Add(func(item));
+            }
+            return result;
+        }
+
+        
+        public static Q Reduce<T, Q>(this List<T> list,Q initial, Func<T, Q , Q> r)
+        {
+            Q result = initial;
+            foreach (T item in list)
+            {
+                result = r(item, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Map and Reduce/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_And_Reduce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(new[] { 1, 2, 3, 4 });
+
+
+            List<string> listString = list.Map<string, int>((x) => x.ToString());
+
+
+            string T = list.Reduce<int, string>("", (x, y) =>
+
+             {
+                 int z = 0;
+                 foreach (var item in list)
+                 {
+                     z += item;
+                 }
+                 return z.ToString();
+             });
+
+
+
+        }
+    }
+}
