3b4f0b60695a66aacab2e2b49e66159a942e9ef1
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-05-21 14:44:57 +0400

    BinaryTree

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class BinaryTree
+    {
+        public int? Root;     
+        public BinaryTree RightNext;
+        public BinaryTree LeftNext;
+        
+        public void Print(BinaryTree c) 
+        {
+            Console.Write(c.Root+"  ");
+            if (c.LeftNext != null)
+            {
+                Print(c.LeftNext);
+                
+            }
+            
+            if (c.RightNext != null)
+            {
+                Print(c.RightNext);
+               
+            }
+           
+        }
+        private void AddLeft(BinaryTree LeftNext, int x)
+        {
+            if (LeftNext.Root == null)
+            {
+                LeftNext.Root = x;
+                return;
+            }
+            else
+            {
+
+                if (x <= LeftNext.Root)
+                {
+                    if (LeftNext.LeftNext == null)
+                    {
+                        LeftNext.LeftNext = new BinaryTree();
+                    }
+                    AddLeft(LeftNext.LeftNext, x);
+                }
+                else
+                {
+                    if (LeftNext.RightNext == null)
+                    {
+                        LeftNext.RightNext = new BinaryTree();
+                    }
+                    AddRight(LeftNext.RightNext, x);
+                }
+            }
+        }
+        private void AddRight(BinaryTree RightNext, int x)
+        {
+            if (RightNext.Root == null)
+            {
+                RightNext.Root = x;
+                return;
+            }
+            else
+            {
+
+                if (x <= RightNext.Root)
+                {
+                    if (RightNext.LeftNext == null)
+                    {
+                        RightNext.LeftNext = new BinaryTree();
+                    }
+                    AddLeft(RightNext.LeftNext, x);
+                }
+                else
+                {
+                    if (RightNext.RightNext == null)
+                    {
+                        RightNext.RightNext = new BinaryTree();
+                    }
+                    AddRight(RightNext.RightNext, x);
+                }
+            }
+        }
+        public void Add(int x)
+        {
+            if (Root == null)
+            {
+                Root = x;
+                return;
+            }
+            else
+            {
+                if (x <= Root)
+                {
+                    if (LeftNext == null)
+                    {
+                        LeftNext = new BinaryTree();
+                        AddLeft(LeftNext, x);
+                    }
+                    else
+                    {
+                        AddLeft(LeftNext, x);
+                    }
+                    return;
+                }
+                else
+                {
+                    if (RightNext == null)
+                    {
+                        RightNext = new BinaryTree();
+                        AddRight(RightNext, x);
+                    }
+                    else
+                    {
+                        AddRight(RightNext, x);
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ BinaryTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree Tree = new BinaryTree();
+            Tree.Add(10);
+            Tree.Add(9);
+            Tree.Add(8);
+            Tree.Add(7);
+            Tree.Add(11);
+            Tree.Add(6);
+            Tree.Add(12);
+            Tree.Print(Tree);
+        }
+    }
+}
