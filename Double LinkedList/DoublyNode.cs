76b0d8dbb13af9284b1ec6b66796583eb2c4c03d
NarekXachatryan <50409595+NarekXachatryan@users.noreply.github.com>
2019-06-03 14:32:05 +0400

    Double LinkedList

---
+++ Double LinkedList/DoublyNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_2
+{
+    class DoublyNode<T>
+    {
+        public T data { get; set; }
+        public DoublyNode<T> Previous { get; set; }
+        public DoublyNode<T> Next { get; set; }
+        public DoublyNode(T data)
+        {
+            this.data = data;
+        }
+    }
+}
